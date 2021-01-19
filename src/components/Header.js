579bf5f2538d581f7a22311ece4e3fc057cef065
Adebayo Temitope <adecancode@gmail.com>
2021-01-19 19:55:21 +0100

    +

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default function Header() {
     return(
         <div className="p-5 mb-16">
-            <p className="font-semibold text-2xl text-purple-500 text-center">Enye Tech Records</p>
+            <p className="font-semibold text-2xl text-purple-500 text-center">Test Records</p>
         </div>
     )
 }
